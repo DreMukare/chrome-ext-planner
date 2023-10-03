03f596913bdd4b82f9ff417a1584f946e87f7a48
DreMukare <dremukare@gmail.com>
2023-10-04 00:23:15 +0300

    🚝wip: setting up global state management with  mobx-state-tree

--- src/stores/RootStore.ts
+++ src/stores/RootStore.ts
@@ -1,3 +1,28 @@
-class RootStore {}
+import { types, getSnapshot, applySnapshot } from 'mobx-state-tree';
+import AuthStore from './AuthStore';
+// import PlanStore from './PlanStore';
+// import ThemeStore from './ThemeStore';
 
-export default RootStore;
+export const RootStore = types
+	.model({
+		storeName: 'rootStore',
+		authStore: types.optional(AuthStore, () => AuthStore.create()),
+		// planStore: types.optional(PlanStore, () => PlanStore.create()),
+		// profileStore: types.optional(ProfileStore, () => ProfileStore.create()),
+		// themeStore: types.optional(ThemeStore, () => ThemeStore.create())
+	})
+	.actions((self) => {
+		let initialSnapshot: any = undefined;
+		function afterCreate() {
+			initialSnapshot = getSnapshot(self);
+		}
+
+		function resetStore() {
+			applySnapshot(self, initialSnapshot); // set store to default
+		}
+
+		return {
+			afterCreate,
+			resetStore,
+		};
+	});
