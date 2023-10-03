a422439bd57b6bd9d6cb5fda840c75d3ac19d7eb
DreMukare <dremukare@gmail.com>
2023-10-03 23:13:09 +0300

    🚝wip: changeable background image with state

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -1,13 +1,16 @@
 import { FunctionComponent, PropsWithChildren, useState } from 'react';
 
 const Background: FunctionComponent<PropsWithChildren> = ({ children }) => {
-	const [bgImg, setBgImg] = useState(
-		'https://i.postimg.cc/cLjZm9RS/default-01.jpg'
-	);
+	const [bgImg, setBgImg] = useState('');
 
 	return (
 		<div
-			className={`fixed top-0 left-0 w-[100vw] h-[100vh] bg-[url('https://i.postimg.cc/cLjZm9RS/default-01.jpg')] bg-no-repeat bg-cover bg-center bg-fixed `}
+			style={{
+				backgroundImage: `url(${
+					bgImg !== '' ? bgImg : 'https://i.postimg.cc/cLjZm9RS/default-01.jpg'
+				})`,
+			}}
+			className={`fixed top-0 left-0 w-[100vw] h-[100vh]  bg-no-repeat bg-cover bg-center bg-fixed `}
 		>
 			{children}
 		</div>
