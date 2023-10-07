869c5f09d4c54739485c5d40a1a138223de6791e
DreMukare <dremukare@gmail.com>
2023-10-07 21:12:13 +0300

    minor: added tanstack-query provider to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-// import { useEffect, useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Background from './components/Background';
 import rootStore from './stores/RootStore';
 import Login from './views/Login';
@@ -6,28 +6,22 @@ import Main from './views/Main';
 import SignUp from './views/SignUp';
 import { observer } from 'mobx-react';
 
+const queryClient = new QueryClient();
+
 function App() {
 	const currentView = rootStore.uiStore.currentView;
+	let viewComponent;
 
-	if (currentView === 'main') {
-		return (
-			<Background>
-				<Main />
-			</Background>
-		);
-	}
+	if (currentView === 'main') viewComponent = <Main />;
 
-	if (currentView === 'signup') {
-		return (
-			<Background>
-				<SignUp />
-			</Background>
-		);
-	}
+	if (currentView === 'signup') viewComponent = <SignUp />;
+
+	if (currentView === 'login') viewComponent = <Login />;
+
 	return (
-		<Background>
-			<Login />
-		</Background>
+		<QueryClientProvider client={queryClient}>
+			<Background>{viewComponent}</Background>
+		</QueryClientProvider>
 	);
 }
 
